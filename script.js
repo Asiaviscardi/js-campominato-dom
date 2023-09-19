5afd09c6fd0a017dadcd58c2fa27959ced9dcf5e
Asiaviscardi <asia.viscardii@gmail.com>
2023-09-19 17:18:51 +0200

    termine

---
+++ script.js
@@ -0,0 +1,20 @@
+/*constanti e variabili*/
+
+let container = document.querySelector("#container")
+
+const bombe = 16
+
+reset ();
+
+
+
+function griglia(){
+    let difficolta = document.value
+    let 
+
+};
+
+function reset(){
+
+    container.innerHTML=('');
+}
